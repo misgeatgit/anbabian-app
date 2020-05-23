cc58224f3c409448d70b6998c6e541f985e430fd
Misgana Bayetta Belachew <misgana.bayetta@gmail.com>
2020-05-24 02:08:27 +0800

    Audio player WIP

--- src/components/BarMusicPlayer.js
+++ src/components/BarMusicPlayer.js
@@ -6,18 +6,138 @@ import { FontAwesome } from '@expo/vector-icons';
 import { Audio } from 'expo-av';
 import { colors, device, gStyle } from '../constants';
 
+const LOOPING_TYPE_ALL = 0;
+const LOOPING_TYPE_ONE = 1;
+const LOADING_STRING = "... loading ...";
+const BUFFERING_STRING = "...buffering...";
+const RATE_SCALE = 3.0;
+
 class BarMusicPlayer extends React.Component {
   constructor(props) {
     super(props);
-
+    this.index = 0;
+    this.isSeeking = false;
+    this.shouldPlayAtEndOfSeek = false;
+    this.playbackInstance = null;
     this.state = {
+      playbackInstanceName: LOADING_STRING,
+      loopingType: LOOPING_TYPE_ALL,
+      muted: false,
+      playbackInstancePosition: null,
+      playbackInstanceDuration: null,
+      shouldPlay: false,
+      isPlaying: false,
+      isBuffering: false,
+      isLoading: true,
+      fontLoaded: false,
+      shouldCorrectPitch: true,
+      volume: 1.0,
+      rate: 1.0,
+      poster: false,
+      useNativeControls: false,
+      fullscreen: false,
+      throughEarpiece: false,
+      // UI related
       favorited: false,
-      paused: true
+      paused: true,
     };
-    this.soundObject = new Audio.Sound();
     this.toggleFavorite = this.toggleFavorite.bind(this);
     this.togglePlay = this.togglePlay.bind(this);
   }
+
+  componentDidMount() {
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      staysActiveInBackground: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false
+    });
+    // Create PlayBackInstance object
+    this._loadNewPlaybackInstance(false);
+  }
+  async _loadNewPlaybackInstance(playing) {
+    if (this.playbackInstance != null) {
+      await this.playbackInstance.unloadAsync();
+      // this.playbackInstance.setOnPlaybackStatusUpdate(null);
+      this.playbackInstance = null;
+    }
+    const { song } = this.props
+    const source = { uri: song.audio };
+    const initialStatus = {
+      shouldPlay: playing,
+      rate: this.state.rate,
+      shouldCorrectPitch: this.state.shouldCorrectPitch,
+      volume: this.state.volume,
+      isMuted: this.state.muted,
+      isLooping: this.state.loopingType === LOOPING_TYPE_ONE
+      // // UNCOMMENT THIS TO TEST THE OLD androidImplementation:
+      // androidImplementation: 'MediaPlayer',
+    };
+
+    const { sound, status } = await Audio.Sound.createAsync(
+      source,
+      initialStatus,
+      this._onPlaybackStatusUpdate
+    );
+    this.playbackInstance = sound;
+
+    this._updateScreenForLoading(false);
+  }
+  
+  _onPlaybackStatusUpdate = status => {
+    if (status.isLoaded) {
+      this.setState({
+        playbackInstancePosition: status.positionMillis,
+        playbackInstanceDuration: status.durationMillis,
+        shouldPlay: status.shouldPlay,
+        isPlaying: status.isPlaying,
+        isBuffering: status.isBuffering,
+        rate: status.rate,
+        muted: status.isMuted,
+        volume: status.volume,
+        loopingType: status.isLooping ? LOOPING_TYPE_ONE : LOOPING_TYPE_ALL,
+        shouldCorrectPitch: status.shouldCorrectPitch
+      });
+      if (status.didJustFinish && !status.isLooping) {
+        //TODO start playing here?
+      }
+    } else {
+      if (status.error) {
+        console.log(`FATAL PLAYER ERROR: ${status.error}`);
+      }
+    }
+  };
+  
+  _updateScreenForLoading(isLoading) {
+    if (isLoading) {
+      this.setState({
+        isPlaying: false,
+        playbackInstanceName: LOADING_STRING,
+        playbackInstanceDuration: null,
+        playbackInstancePosition: null,
+        isLoading: true
+      });
+    } else {
+      const { song } = this.props
+      this.setState({
+        playbackInstanceName: song.title,
+        isLoading: false
+      });
+    }
+  }
+
+  _onPlayPausePressed = () => {
+    if (this.playbackInstance != null) {
+      if (this.state.isPlaying) {
+        this.playbackInstance.pauseAsync();
+      } else {
+        this.playbackInstance.playAsync();
+      }
+    }
+  };
 
   toggleFavorite() {
     this.setState(prev => ({
@@ -27,6 +147,13 @@ class BarMusicPlayer extends React.Component {
 
   async togglePlay() {
     this.setState(prev => ({
+      isPlaying: !prev.isPlaying
+    }));
+    this._onPlayPausePressed()
+  }
+  
+  async togglePlayX() {
+    this.setState(prev => ({
       paused: !prev.paused
     }));
     const { paused } = this.state;
@@ -35,13 +162,13 @@ class BarMusicPlayer extends React.Component {
     if (!paused && song.audio) {
       console.log('AUDIO exists.');
       try {
-        await this.soundObject.loadAsync({uri: song.audio});
+        await this.soundObject.loadAsync({ uri: song.audio });
         await this.soundObject.playAsync();
       } catch (error) {
         console.log(`ERROR: Unable to load and play ${song.title}`);
       }
-    } 
-    else if(paused && song.audio){
+    }
+    else if (paused && song.audio) {
       try {
         await this.soundObject.pauseAsync();
       } catch (error) {
