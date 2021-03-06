bead07db691112bdbc83e53cc42755bafbefd7c1
Misgana Bayetta <misgana.bayetta@gmail.com>
2021-03-07 00:44:58 +0800

    Added test case

--- src/test/AudioBook.test.js
+++ src/test/AudioBook.test.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-undef */
 import { fetchMockData } from '../data/AudioBook';
 
-test('total number of mock books equals 60', () => {
+test('total number of mock books equals 60.', () => {
   expect(fetchMockData().length).toBe(60);
+});
+
+test('Book by the title "Ancient and Modern Celebrated Freethinkers" has 24 chapters.', () => {
+  expect(
+    fetchMockData().filter(book => {
+      return book.title.trim() === 'Ancient and Modern Celebrated Freethinkers';
+    })[0].chapters.length
+  ).toBe(24);
 });
